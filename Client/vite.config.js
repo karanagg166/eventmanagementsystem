64433237e43ab70c96d7a998557dc6363f55690c
karanagg166 <22bcs130.@iiitdmj.ac.in>
2024-09-14 04:22:24 +0530

    hi

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,37 +1,7 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import sitemap from "vite-plugin-sitemap";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://iluvcollege.vercel.app/", // Replace with your domain
-      outDir: "dist", // Output directory where the sitemap will be generated
-    }),
-    sentryVitePlugin({
-      org: "poojan-goyani",
-      project: "javascript-react",
-      authToken: process.env.VITE_SENTRY_AUTH_TOKEN,
-    }),
-  ],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
-});
+  plugins: [react()],
+})
